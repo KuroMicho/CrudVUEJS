0cfbd8739ce4a430018093ff6fa15db32c1ad30d
KuroMicho <kevin0552009.kr@gmail.com>
2019-03-12 20:43:18 -0500

    HOLA

---
+++ index2.js
@@ -0,0 +1,16 @@
+let vm = new Vue({
+    el: `#app`,
+    data:
+    {
+        show_now: false,
+        nombre: " ",
+        apellido: " "
+
+    },
+    methods: {
+        guardar()
+        {
+            alert("empezando a guardar")
+        }
+    }
+})
